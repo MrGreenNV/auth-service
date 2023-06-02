23c006e7f64cf2bbccdc54028022956560b28236
MrGreenNV <averkievnv@gmail.com>
2023-06-02 20:28:29 +0300

    update PersonDetails.java add PersonDetailsService.java, PeopleRepository.java, SecurityConfig.java, AuthProviderImpl.java

---
+++ src/main/java/ru/averkiev/authservice/services/PersonDetailsService.java
@@ -0,0 +1,33 @@
+package ru.averkiev.authservice.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.averkiev.authservice.models.Person;
+import ru.averkiev.authservice.repositories.PeopleRepository;
+import ru.averkiev.authservice.security.PersonDetails;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByUsername(username);
+
+        if (person.isEmpty()) {
+            throw new UsernameNotFoundException("Пользователь не найден!");
+        }
+
+        return new PersonDetails(person.get());
+    }
+}
