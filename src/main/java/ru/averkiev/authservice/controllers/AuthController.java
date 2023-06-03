73e54ae1db8d174306dfb614aa8c559242c0e358
MrGreenNV <averkievnv@gmail.com>
2023-06-03 22:27:42 +0300

    add AuthController.java, RefreshJwtRequest.java

---
+++ src/main/java/ru/averkiev/authservice/controllers/AuthController.java
@@ -0,0 +1,36 @@
+package ru.averkiev.authservice.controllers;
+
+import jakarta.security.auth.message.AuthException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.averkiev.authservice.models.RefreshJwtRequest;
+import ru.averkiev.authservice.security.JwtRequest;
+import ru.averkiev.authservice.security.JwtResponse;
+import ru.averkiev.authservice.services.AuthService;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest authRequest) throws AuthException {
+        final JwtResponse token = authService.login(authRequest);
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("token")
+    public ResponseEntity<JwtResponse> getNewAccessToken(@RequestBody RefreshJwtRequest request) {
+        final JwtResponse token = authService.getAccessToken(request.getRefreshToken());
+        return ResponseEntity.ok(token);
+    }
+
+}
