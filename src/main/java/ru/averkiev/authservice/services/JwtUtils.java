ede189a6aa8ac5ee5345843191fd4eeb3da4b513
MrGreenNV <averkievnv@gmail.com>
2023-06-26 19:10:18 +0300

    update JwtUtils.java

--- src/main/java/ru/averkiev/authservice/services/JwtUtils.java
+++ src/main/java/ru/averkiev/authservice/services/JwtUtils.java
@@ -1,16 +1,29 @@
 package ru.averkiev.authservice.services;
 
 import io.jsonwebtoken.Claims;
+import lombok.NoArgsConstructor;
 import ru.averkiev.authservice.models.Role;
 import ru.averkiev.authservice.security.JwtAuthentication;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
 public final class JwtUtils {
 
     public static JwtAuthentication generate(Claims claims) {
         final JwtAuthentication jwtInfoToken = new JwtAuthentication();
-        jwtInfoToken.setRole(claims.get("role", String.class));
-        jwtInfoToken.setEmail(claims.get("email", String.class));
+        jwtInfoToken.setRoles(getRoles(claims));
+        jwtInfoToken.setFirstname(claims.get("firstname", String.class));
         jwtInfoToken.setUsername(claims.getSubject());
         return jwtInfoToken;
+    }
+
+    private static Set<Role> getRoles(Claims claims) {
+        final List<String> roles = claims.get("roles", List.class);
+        return roles.stream()
+                .map(Role::valueOf)
+                .collect(Collectors.toSet());
     }
 }
